c8720eeda5f5e62c271f36d459504d4435d54b79
Similachi Andrei-Felix <68543873+SimilachiAndrei@users.noreply.github.com>
2024-04-09 17:56:41 +0300

    Delete proiect directory

--- proiect/scripts/toggle.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    var menuBar = document.querySelector(".menu-bar");
-    menu.classList.toggle("show");
-    menuBar.classList.toggle("change");
-}
