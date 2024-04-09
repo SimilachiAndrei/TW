809ab51a578386839d084d998382198ba46b75a5
Similachi Andrei-Felix <68543873+SimilachiAndrei@users.noreply.github.com>
2024-04-09 18:00:23 +0300

    Pagini curente

---
+++ proiectWeb/scripts/toggle.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var menuBar = document.querySelector(".menu-bar");
+    menu.classList.toggle("show");
+    menuBar.classList.toggle("change");
+}
